4f6ac15aa0c1a7a6cc363af6d5de3aa9fb296040
wbrydak <brydakwojciech@gmail.com>
2022-05-05 12:53:00 +0200

    allow for batch file processing

--- app/app.py
+++ app/app.py
@@ -1,3 +1,24 @@
+from multiprocessing.dummy import Pool as ThreadPool 
 from bs4 import BeautifulSoup
+from os import walk
 
-soup = BeautifulSoup.get.text()
+pool = ThreadPool(20)
+
+htmls = []
+for root, dirs, files in walk('./input-files/'):
+  for file in files:
+    if r'.*\.html'.match(file):
+      htmls.append(file)
+
+df = pd.DataFrame(columns=['date', 'title', 'author', 'source', 'wordcount'])
+data_list = []
+
+def crawl_html(html_file):
+  soup = BeautifulSoup(open(html_file), 'html.parser')
+  data_list.append({
+    
+  })
+
+results = pool.map(crawl_html, htmls)
+
+print(df.append(data_list))
