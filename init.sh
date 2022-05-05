8a543824b1406f78c3a330756c0d7a0b860a972d
wbrydak <brydakwojciech@gmail.com>
2022-05-05 15:32:56 +0200

    update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CONTAINER_NAME="local.dev/thunderbird-html-mail-data-processor:latest"
 docker build . -f Dockerfile -t "${CONTAINER_NAME}"
+docker run -d -it --mount type=bind,source="$(pwd)"/app/input-files,target=/app,readonly \
